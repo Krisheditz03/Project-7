abc995bc3a072f7d12e1552434a3cd7b74d5bf6b
Krisheditz03 <krishpanwar08@gmail.com>
2023-08-19 21:46:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const getColor= () => {
+    // Hex Code
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    console.log(randomNumber,randomCode);
+    document.body.style.background = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+
+    navigator.clipboard.writeText(randomCode)
+}
+// event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+// initial call
+getColor();
